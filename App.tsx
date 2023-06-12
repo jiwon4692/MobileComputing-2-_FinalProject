610258bf0faf574bc4eb3236aad567417def9504
jiwon4692 <103973797+jiwon4692@users.noreply.github.com>
2023-06-13 06:10:39 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, { useState } from 'react';
+import AppControlFlow from './src/AppControlFlow';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import Toast from 'react-native-toast-message';
+
+
+const App = () => {
+  return <AppControlFlow />;
+  
+};
+
+export default App;
